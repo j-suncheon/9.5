c80e3ebaba537926baf7bc69f785901d67a4bca3
j-suncheon <whaql78@gamil.com>
2022-09-19 14:41:23 +0900

    9.5_C#

---
+++ CodeFile1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PropertyApp
+{
+    class Fraction
+    {
+        private int number;
+        public int Number
+        {
+            get { return number; }
+            set { number = value; }
+        }
+        private int sum;
+        public int Sum
+        {
+            get { return sum; }
+            set { sum = value; }
+        }
+        public static void Main(string[] args)
+        {
+            Fraction f = new Fraction();
+            f.Number = 10;
+            Console.WriteLine(f.Number);
+            f.Sum = 20;
+            Console.WriteLine(f.Sum);
+        }
+    }
+}
